1be199b1fee1932591950f7cb91eeae61ec9cf20
Pradeep A <36046556+PRADEEP-ANDRU@users.noreply.github.com>
2018-05-11 14:35:42 +0530

    Create given tables in MySQL

---
+++ tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE LOGIN (user_name varchar(8) PRIMARY KEY,
+pass varchar(8),
+age integer(2),
+salary integer(6));
+
+CREATE TABLE MANAG ( user_name varchar(8), 
+vehicle integer(5),
+food integer(5),
+enteratainment integer(5),
+grocery integer(5),
+misc integer(5));
